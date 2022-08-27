fde5ba2997c5375692c91bb0c6971133d016891b
Micael Macedo <micaelm2009@hotmail.com>
2022-08-27 13:18:50 -0300

    feat: add serviços mais adicionados

--- js/script.js
+++ js/script.js
@@ -8,7 +8,6 @@ function exibirMenu(){
 window.addEventListener('resize',function tamanhoNav(){
     var menu = document.getElementById('navMenu');
     if(document.body.scrollWidth < 1000){
-        window.alert("entrei")
         menu.style.display = "block";
     }else{
         menu.style.display = "none";
