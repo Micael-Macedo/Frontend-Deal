16f635055b2f9904001eb208c2e6f885a7070b9d
Isaias Pinheiro <99775246+izal25@users.noreply.github.com>
2022-09-06 16:37:13 -0300

    Add files via upload

---
+++ Frontend-Deal-main/Frontend-Deal-main/js/script.js
@@ -0,0 +1,15 @@
+function exibirMenu(){
+    if(menuItens.style.display == 'block'){
+        menuItens.style.display = 'none'
+    }else{
+        menuItens.style.display = 'block'
+    }
+}
+window.addEventListener('resize',function tamanhoNav(){
+    var menu = document.getElementById('navMenu');
+    if(document.body.scrollWidth < 1000){
+        menu.style.display = "block";
+    }else{
+        menu.style.display = "none";
+    }
+},true);
